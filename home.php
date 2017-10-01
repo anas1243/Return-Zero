338f6fbb49748564e3b108d2372a0e6d7d96e7e7
Sherif Mohamed Saleh <sherief33392@gmail.com>
2017-10-01 16:41:02 -0400

    Add files via upload

---
+++ home.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+   <title>RETURN_ZERO</title>
+    <?php include 'login_proc.php';?>
+	<link type="text/css" rel="stylesheet" href="loginsty.css">
+	<link type="text/css" rel="stylesheet" href="navsty.css">
+	
+	<style >
+    .error {color: #FF0000;font-size: 20px;text-align: right;}
+  </style>
+
+</head>
+
+<body>
+	
+<header>
+
+    <a href="login.php" id="logo">
+                     <img src="rezero.png" height="55px" width=" 80px">
+                </a>
+  
+  <nav>
+    <ul>  
+      <li><a href="home.php">Home</a></li>
+      <li><a href="login.php">LOGIN</a></li>
+      <li><a href="register.php">REGISTRATION</a></li>
+ 
+    </ul>
+  </nav>
+</header>
+	
+<div class='wrap'>
+  <img src="rezero.png" id="icon" content="Home" height="300px" width=" 500px">
+  </div>
+	</body>
+</html>
